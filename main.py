2b09cc819ff2722bdb08569ba1f798f43357a3bf
kIwwwww1 <danil29070@gmail.com>
2025-11-21 16:38:59 +0300

    Создание docker-compose файла

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import uvicorn
+import string
+from secrets import choice
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
+ALPHABET: str = string.ascii_letters + string.digits
+
+def genearate_random_slug() -> str:
+    slug = ''
+    for _ in range(6):
+        slug += choice(ALPHABET)
+    return slug
 
 @app.post('/short_url')
 async def generate_short_url(url: int): 
     return {'True': 'Ваша короткая ссылка'}
 
 @app.get('/{slug}')
-async def redirect_to_url(slug: str): 
+async def redirect_to_url(slug: str):
     return {'True': f'Ваша короткая ссылка {slug}'}
+
 
 
 
