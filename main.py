9cd4f068975eed50aff395e27be4b69300682907
kIwwwww1 <danil29070@gmail.com>
2025-11-21 14:08:35 +0300

    Базовый старт

---
+++ main.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post('/short_url')
+async def generate_short_url(url: int): 
+    return {'True': 'Ваша короткая ссылка'}
+
+@app.get('/{slug}')
+async def redirect_to_url(slug: str): 
+    return {'True': f'Ваша короткая ссылка {slug}'}
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app='main:app', reload=True)
